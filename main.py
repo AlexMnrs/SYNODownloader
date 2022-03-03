23647b068c272c1c73f6d6e510cad4167f1d504f
AlexMnrs <alexmnrs92@gmail.com>
2022-03-03 13:49:52 +0100

    Added option to choose download folder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
-import requests 
+from turtle import down
+import requests
+import os
 
 #DO NOT MODIFY
 LOGIN_API    = 'webapi/auth.cgi?api=SYNO.API.Auth&version=3&method=login&account={}&passwd={}&session=FileStation&format=cookie'
@@ -11,7 +13,7 @@ DOWNLOAD_API = 'webapi/entry.cgi?api=SYNO.FileStation.Download&version=2&method=
 URL  =  'https://nas.quickconnect.to:65375/'
 USER =  'admin'
 PASS =  'password'
-PATH =  'videos'
+PATH =  'your_folder'
 
 def login(url, id, pw):
     sid = 0
@@ -65,18 +67,47 @@ def logout(url, api):
     response = requests.get(url+api)
     return response
 
+def currDir():
+    curr_dir = os.getcwd()
+    return curr_dir
+
+def setDownloadsFolder():
+    user_folder = os.environ['USERPROFILE']
+    os.chdir("{}\{}".format(user_folder, "Downloads"))
+    
+    print("[*] Your current directory is: {}".format(currDir()))
+    
+    downloads_folder = input("[*] Enter the name of the folder to download files: ")
+    while os.path.isdir(downloads_folder):
+         dec = input("[*] Folder already exists. Do you want to download files in -> {}? (y/n)".format(downloads_folder))
+         if dec == 'y':
+             os.chdir("{}\{}".format(currDir(), downloads_folder))
+             print("[*] Download folder set to: {}\n".format(currDir()))
+             break
+         else:
+            downloads_folder = input("[*] Enter different folder name than {}: ".format(downloads_folder))
+            
+    if not downloads_folder in currDir():
+        os.mkdir(downloads_folder)
+        print("[*] Folder {} created.".format(downloads_folder))
+        os.chdir("{}\{}".format(currDir(), downloads_folder))
+        print("[*] Current directory: {}\n".format(currDir()))
+
 if __name__ == '__main__':
+    os.system('cls')
+        
     try:
         sid = login(URL, USER, PASS)
+        setDownloadsFolder()
         try:
             files = list(URL, PATH, sid)
-            choice = int(input("\n[*] Choose a file (e.g. 6): "))
-
-            if choice in files:
+            while files:
+                choice = int(input("\n[*] Choose a file (e.g. 6): "))
                 verify_choice = str(input("\n[*] You have selected: {}. Are you sure? (y/n): ".format(files.get(choice))))
                 if verify_choice in "y":
                     print("\n[*] Please, wait...\n")
                     download(url=URL, path=PATH, file=files.get(choice), sid=sid)
+                    break     
             else:
                 print("[*] That file doesn't exist.")
         except ValueError:
